6076cb00549e07d921f140ca0afa80f2faa70bfa
cirodkp <ciro.almeida@dkp-it.com.br>
2025-05-10 11:47:36 -0300

    ajuste tolerancia

--- tests/HealthMed.Agenda.Integration.Tests/IntegrationTests/AgendaIntegrationTests.cs
+++ tests/HealthMed.Agenda.Integration.Tests/IntegrationTests/AgendaIntegrationTests.cs
@@ -50,7 +50,7 @@ namespace HealthMed.Agenda.Integration.Tests.IntegrationTests
 
             // Validate Published/Consumed and Inserted Agenda In Database
             AgendaFromDatabase.Should().NotBeNull();
-            AgendaFromDatabase.Select(ag => ag.DataHora).ShouldContainDateCloseTo(publishedResponseData.DataHora, TimeSpan.FromSeconds(25));
+            AgendaFromDatabase.Select(ag => ag.DataHora).ShouldContainDateCloseTo(publishedResponseData.DataHora, TimeSpan.FromMinutes(1));
         }
 
         public async Task Login(string login = "CRMADMIN", string senha = "123456")
