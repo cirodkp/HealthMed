b47266e570d26796ea8b6e72213b9ac8ebbab1ec
brunodahora <brunoshora@gmail.com>
2025-05-11 01:41:36 -0300

    Teste deploy versão anterior dos testes de integração agenda

--- tests/HealthMed.Agenda.Integration.Tests/Extensions/DateTimeAssertionsExtensions.cs
+++ tests/HealthMed.Agenda.Integration.Tests/Extensions/DateTimeAssertionsExtensions.cs
@@ -2,18 +2,15 @@
 
 namespace HealthMed.Agenda.Integration.Tests.Extensions
 {
-   
-        public static class DateTimeAssertionsExtensions
+    public static class DateTimeAssertionsExtensions
+    {
+        public static void ShouldContainDateCloseTo(this IEnumerable<DateTime> lista, DateTime esperado, TimeSpan tolerancia)
         {
-            public static void ShouldContainDateCloseTo(this IEnumerable<DateTime> lista, DateTime esperado, TimeSpan tolerancia)
-            {
-                var esperadoUtc = esperado.ToUniversalTime();
-
-                lista.Any(data =>
-                    data.ToUniversalTime() >= esperadoUtc - tolerancia &&
-                    data.ToUniversalTime() <= esperadoUtc + tolerancia
-                ).Should().BeTrue($"Esperava-se um DateTime dentro de {tolerancia.TotalSeconds} segundos de {esperadoUtc}.");
-            }
+            lista.Any(data =>
+                data >= esperado - tolerancia &&
+                data <= esperado + tolerancia
+            ).Should().BeTrue($"Esperava-se um DateTime dentro de {tolerancia.TotalSeconds} segundos de {esperado}.");
         }
+    }
     
 }
